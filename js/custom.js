93e29dbbad7937f27dcef81c5ff3fe5b7a20166b
senuLab <kondo.issei11@gmail.com>
2021-01-01 19:35:29 +0900

    ハンバーガー作成

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".offcanvas-right").hiraku({
+        btn:"#offcanvas-btn-right",
+        direction:"right"
+    });
+
+    /* $hamburger = $(".js-header__hamburger-btn");
+
+    $hamburger.on("click",function(){
+        $(".js-header__hamburger-btn--open").toggleClass("is-active");
+        $(".js-header__hamburger-btn--close").toggleClass("is-active");
+    }); */
+});
